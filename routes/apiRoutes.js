00b0f929a45955ea9962ff56096b3c6e7f647b35
Tung Tran <ttran1185@gmail.com>
2020-10-14 20:54:20 -0700

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,8 @@
 const router = express.router();
 const express = require("express");
 const workout = require("../models/workout.js");
+
+router.get("api/workout", (req, res) => {
+    workout.create({}).then(data => { res.json(data)}).catch( err => res.json(err));
+});
+

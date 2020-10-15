dfe1a7da934c57edbbb94ed6cf6f01f7162e4944
Tung Tran <ttran1185@gmail.com>
2020-10-14 20:50:17 -0700

    update

---
+++ routes/apiRoutes.js
@@ -0,0 +1,3 @@
+const router = express.router();
+const express = require("express");
+const workout = require("../models/workout.js");
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = function(router) {
+    router.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, ".../index.html"));
+    });
+
+    router.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, ".../stats.html"));
+    });
+
+    router.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, ".../exercise.html"));
+    });
+
+};

3684b4930d7d0ed67c0f2b6a72d19c93b746a9f4
Tung Tran <ttran1185@gmail.com>
2020-10-14 20:17:43 -0700

    update

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
 
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness_tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));

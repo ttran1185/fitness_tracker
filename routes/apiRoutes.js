483d3c42d07afaf9be72a14c7dc15fa43e56a561
Tung Tran <ttran1185@gmail.com>
2020-10-26 09:13:25 -0700

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,14 +3,21 @@ const express = require("express");
 const workout = require("../models/workout.js");
 
 
-module.exports =function (router){
+module.exports =function (router) {
 router.post("api/workout", (req, res) => {
     workout.create({}).then(data => { res.json(data)}).catch( err => res.json(err));
 });
 
-router.put("api/workout/:id", (req, res) => {
-    workout.findByIdAndUpdate({}).then(data => { res.json(data)}).catch( err => res.json(err));
-});
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then(data => res.json(data))
+      .catch(err => {
+        res.json(err);
+      });
 
 router.get("api/workout", (req, res) => {
     workout.find({}).then(data => { res.json(data)}).catch( err => res.json(err));
